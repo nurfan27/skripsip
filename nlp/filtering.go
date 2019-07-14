fa746ac64da26f3bbf733fb0f586e321a41b7ad3
Nurfan Ramadhandi <nurfan.ramadhandi@qasir.id>
2019-07-14 17:29:26 +0700

    filtering running well without implement levenshtein deistance

--- nlp/filtering.go
+++ nlp/filtering.go
@@ -1,14 +1,50 @@
 package nlp
 
 import (
+	"bufio"
 	"log"
+	"os"
+	"strings"
 )
 
-func WordList(params map[string]string) string {
+func WordList(params map[string]string) []string {
 	log.Println("wordlist : ")
-	for index, _ := range params {
-		log.Println(index)
+
+	var wordkey []string
+
+	lines, err := readLines("wordlist.txt")
+	if err != nil {
+		log.Fatalf("readLines: %s", err)
 	}
 
-	return ""
+	for index, _ := range params {
+		for _, line := range lines {
+			// fmt.Println(i, line)
+			log.Println(index, line)
+			if strings.TrimSpace(index) == strings.TrimSpace(line) {
+				log.Println("sama bro ")
+				wordkey = append(wordkey, index)
+			}
+		}
+
+	}
+
+	return wordkey
+}
+
+// readLines reads a whole file into memory
+// and returns a slice of its lines.
+func readLines(path string) ([]string, error) {
+	file, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	var lines []string
+	scanner := bufio.NewScanner(file)
+	for scanner.Scan() {
+		lines = append(lines, scanner.Text())
+	}
+	return lines, scanner.Err()
 }
